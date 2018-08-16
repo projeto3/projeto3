f2b3f2bd4d0202a173d3d71aed53dc2c0ae5860b
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-16 16:12:29 -0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "$VAR_ACCESS_KEY"
-  secret_key = "$VAR_SECRET_KEY"
-  region     = "$VAR_REGION"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-04169656fea786776"
   instance_type = "t2.micro"
