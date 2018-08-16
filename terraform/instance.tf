2fc5167f8c9719dd1f06e18658e031c1aac467c3
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-16 16:19:23 -0300

    Update instance.tf

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "projeto3" {
-  ami           = "ami-04169656fea786776"
+  ami           = "${lookup(var.AMIS,var.AWS_REGION)}"
   instance_type = "t2.micro"
   subnet_id     = "subnet-04ddf27ca8d47df64"
   key_name	    = "projeto3"
