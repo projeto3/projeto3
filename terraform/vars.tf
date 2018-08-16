c9d33909a58bdbc3f3478990a058942a050774e5
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-16 17:13:33 -0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,11 +3,4 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
-variable "AMIS" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
-  }
-}
+
