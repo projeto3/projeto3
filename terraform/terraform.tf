16b7b40707421e1771b30224ae87d22ba3a6b764
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-16 16:14:11 -0300

    Delete terraform.tf

--- terraform/terraform.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-04169656fea786776"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-04ddf27ca8d47df64"
-  key_name	= "projeto3"
-}
