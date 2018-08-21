85c803df2b1bdf2ea79f4efbe1d76fbbcbec0928
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-20 21:35:23 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  access_key = "${AWS_ACCESS_KEY}"
-  secret_key = "${AWS_SECRET_KEY}"
-  region     = "${AWS_REGION}"
-}
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
