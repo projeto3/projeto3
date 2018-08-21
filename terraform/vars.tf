c5ac55f40b1ac2c82b651abe02dd508452947f19
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-21 18:56:34 -0300

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,13 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "eu-west-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
