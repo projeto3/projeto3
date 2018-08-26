f74c12e52f9cae19260dfd7fd1e1099d7a266bef
projeto3 <42242436+projeto3@users.noreply.github.com>
2018-08-26 09:12:59 -0300

    Update index.php

--- docker/www/index.php
+++ docker/www/index.php
@@ -1,15 +1,15 @@
 <?php
-$dsn = 'mysql:dbname=projeto_docker;host=mysql-local;port=3306';
+$dsn = 'mysql:dbname=pepiline_devops;host=mysql-local;port=3306';
 $user = 'docker';
 $password = 'docker';
 try {
     $pdo = new PDO($dsn, $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $result = $pdo->query("SHOW TABLES LIKE 'contato'");
+    $result = $pdo->query("SHOW TABLES LIKE 'contatos'");
     $table = $result->fetchAll();
     if (!count($table)) {
         $pdo->exec(
-            "CREATE TABLE contato(\n" .
+            "CREATE TABLE contatos(\n" .
             "    id INT AUTO_INCREMENT NOT NULL,\n" .
             "    firstName VARCHAR(250) NOT NULL,\n" .
             "    lastName VARCHAR(250) NOT NULL,\n" .
@@ -17,14 +17,14 @@ try {
             "    PRIMARY KEY (id)\n" .
             ")");
     }
-    $statement = $pdo->prepare("INSERT INTO contato (firstName, lastName, email) VALUES (:firstName, :lastName, :email)");
+    $statement = $pdo->prepare("INSERT INTO contatos (firstName, lastName, email) VALUES (:firstName, :lastName, :email)");
     $statement->bindParam(':firstName', $firstName);
     $statement->bindParam(':lastName', $lastName);
     $statement->bindParam(':email', $email);
     // insert a row
-    $firstName = "Anderson";
-    $lastName = "Farias";
-    $email = "john@email.com";
+    $firstName = "Admin";
+    $lastName = "Admin";
+    $email = "admin@email.com";
     $statement->execute();
     // insert another row
     $firstName = "Mary";
@@ -38,7 +38,7 @@ try {
     $statement->execute();
     echo '<pre>',
         json_encode(
-            $pdo->query('SELECT id, firstName, lastName, email FROM contato')->fetchAll(PDO::FETCH_ASSOC),
+            $pdo->query('SELECT id, firstName, lastName, email FROM contatos')->fetchAll(PDO::FETCH_ASSOC),
                 JSON_PRETTY_PRINT
         );
 } catch (PDOException $e) {
