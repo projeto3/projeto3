8d36480b9bb9dd93b3e377ec1f159c6a33c8939b
projeto3 <42242436+projeto3@users.noreply.github.com>
2020-08-04 14:33:55 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,9 +5,9 @@ resource "aws_vpc" "main" {
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
     enable_classiclink = "false"
-    tags {
-        Name = "main"
-    }
+ //   tags {
+ //       Name = "main"
+ //   }
 }
 # Subnets
 resource "aws_subnet" "main-public-1" {
