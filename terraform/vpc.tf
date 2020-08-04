96fbe0c4b023f707a1ddd77e076f98d37cee71de
projeto3 <42242436+projeto3@users.noreply.github.com>
2020-08-04 14:28:15 -0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "main" {
 }
 # Subnets
 resource "aws_subnet" "main-public-1" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-west-1a"
@@ -21,7 +21,7 @@ resource "aws_subnet" "main-public-1" {
     }
 }
 resource "aws_subnet" "main-public-2" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
     availability_zone = "us-west-1c"
@@ -32,7 +32,7 @@ resource "aws_subnet" "main-public-2" {
 }
 
 resource "aws_subnet" "main-private-1" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
     availability_zone = "us-west-1a"
@@ -45,7 +45,7 @@ resource "aws_subnet" "main-private-1" {
 
 # Internet GW
 resource "aws_internet_gateway" "main-gw" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
 
     tags {
         Name = "main"
@@ -54,10 +54,10 @@ resource "aws_internet_gateway" "main-gw" {
 
 # route tables
 resource "aws_route_table" "main-public" {
-    vpc_id = "${aws_vpc.main.id}"
+    vpc_id = aws_vpc.main.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.main-gw.id}"
+        gateway_id = aws_internet_gateway.main-gw.id
     }
 
     tags {
@@ -67,10 +67,10 @@ resource "aws_route_table" "main-public" {
 
 # route associations public
 resource "aws_route_table_association" "main-public-1-a" {
-    subnet_id = "${aws_subnet.main-public-1.id}"
-    route_table_id = "${aws_route_table.main-public.id}"
+    subnet_id = aws_subnet.main-public-1.id
+    route_table_id = aws_route_table.main-public.id
 }
 resource "aws_route_table_association" "main-public-2-a" {
-    subnet_id = "${aws_subnet.main-public-2.id}"
-    route_table_id = "${aws_route_table.main-public.id}"
+    subnet_id = aws_subnet.main-public-2.id
+    route_table_id = aws_route_table.main-public.id
 }
